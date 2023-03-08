db34ee198a21bba61976408d4473b1824eedd3f7
Ivan Pribec <ivan.pribec@gmail.com>
2023-03-08 02:34:32 +0100

    Modify SGEMM calls

--- linalg_mkl.f90
+++ linalg_mkl.f90
@@ -10,14 +10,18 @@ contains
     real(sp), intent(in) :: A(:,:), B(:,:)
     real(sp), intent(out) :: C(:,:)
     
-    integer :: m, n, k
+    integer :: m, n, k, lda, ldb, ldc
     external :: sgemm
 
     m = size(A,1)
     n = size(B,2)
     k = size(A,2)
 
-    call sgemm('N','N',m,n,k,1.0_sp,A,size(A,1),B,size(B,1),0.0_sp,C,size(C,1))
+    lda = size(A,1)
+    ldb = size(B,1)
+    ldc = size(C,1)
+    
+    call sgemm('N','N',m,n,k,1.0_sp,A,lda,B,ldb,0.0_sp,C,ldc)
 
     end subroutine
 
@@ -25,14 +29,18 @@ contains
     ! C = matmul(transpose(A), B)
     real(sp), intent(in) :: A(:,:), B(:,:)
     real(sp), intent(out) :: C(:,:)
-    integer :: m, n, k
+    integer :: m, n, k, lda, ldb, ldc
     external :: sgemm
 
     m = size(A,1)
     n = size(B,2)
     k = size(A,2)
 
-    call sgemm('T','N',m,n,k,1.0_sp,A,size(A,1),B,size(B,1),0.0_sp,C,size(C,1))
+    lda = size(A,1)
+    ldb = size(B,1)
+    ldc = size(C,1)
+
+    call sgemm('T','N',m,n,k,1.0_sp,A,lda,B,ldb,0.0_sp,C,ldc)
 
     end subroutine
 
