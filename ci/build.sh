d3c03e5c426ae0ae738daad5028210269a15b185
Ondřej Čertík <ondrej@certik.us>
2023-07-22 16:37:20 -0600

    CI: Test downloading the model directly

--- ci/build.sh
+++ ci/build.sh
@@ -15,5 +15,9 @@ cmake -DFASTGPT_BLAS=OpenBLAS .
 make
 time OMP_NUM_THREADS=1 OPENBLAS_NUM_THREADS=1 ./gpt2
 
+rm model.dat
+curl -O model.dat -L https://huggingface.co/datasets/certik/fastGPT/resolve/main/model_fastgpt_124M_v1.dat
+time OMP_NUM_THREADS=1 OPENBLAS_NUM_THREADS=1 ./gpt2
+
 rm gpt2
 python pt.py
