fafe991a31975afb1ee00fa68756678cdeefc98d
Ondřej Čertík <ondrej@certik.us>
2023-07-23 11:02:17 -0600

    Rework input handling

--- gpt2.f90
+++ gpt2.f90
@@ -236,7 +236,6 @@ end do
 x = layer_norm(x, lnf_g, lnf_b, 1e-5)
 !y = matmul(transpose(wte), x)
 call matmul_2d_t(wte, x, y)
-!stop "OK"
 end subroutine
 
 function generate(n_tokens_to_generate, m, &
@@ -254,22 +253,21 @@ real(sp), allocatable :: logits(:,:)
 integer :: i
 integer :: n_seq2, n_seq_x
 integer :: next_id
-integer, allocatable :: input2(:)
+integer :: input2(size(input)+n_tokens_to_generate)
 logical :: use_kv_cache
 real(sp) :: kv_cache(m%n_embd,n_seq+n_tokens_to_generate,2,m%n_layer)
 character(:), allocatable :: output_txt, last_token
-allocate(input2(size(input)))
 if (present(stop_text)) then
     output_txt = ""
 end if
-input2 = input
+input2(:n_seq) = input
 do i = 1, n_tokens_to_generate
     if (use_cache) then
         use_kv_cache = (i > 1) ! Use cache for subsequent tokens
     else
         use_kv_cache = .false.
     end if
-    n_seq2 = size(input2)
+    n_seq2 = n_seq+i-1
     if (use_kv_cache) then
         n_seq_x = 1
     else
@@ -278,16 +276,16 @@ do i = 1, n_tokens_to_generate
     allocate(logits(m%n_vocab, n_seq_x))
     call gpt2(logits, m%n_vocab, m%n_ctx, n_seq2, n_seq_x, m%n_embd, m%n_layer, &
             m%n_head, &
-            input2, &
+            input2(:n_seq2), &
             m%wte, m%wpe, &
             m%mlp_fc_w, m%mlp_fc_b, m%mlp_proj_w, m%mlp_proj_b, &
             m%attn_w, m%attn_b, m%attn_proj_w, m%attn_proj_b, &
             m%ln1_g, m%ln1_b, m%ln2_g, m%ln2_b, m%lnf_g, m%lnf_b, use_kv_cache, kv_cache(:,:n_seq2,:,:))
     next_id = maxloc(logits(:,n_seq_x), dim=1)-1
-    input2 = [input2, next_id]
-    last_token = decode([next_id], m%decoder_idx, &
-        m%decoder_txt, byte_decoder)
-    write(*, fmt="(a)", advance="no") last_token
+    input2(n_seq2+1) = next_id
+    !last_token = decode([next_id], m%decoder_idx, &
+    !    m%decoder_txt, byte_decoder)
+    !write(*, fmt="(a)", advance="no") last_token
     if (present(stop_text)) then
         output_txt = output_txt // last_token
         if (output_txt(len(output_txt)-len(stop_text)+1:len(output_txt)) == stop_text) then
