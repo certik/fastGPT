8ddfd17b99057855c2f79e9f97f7f940e90f8603
Ondřej Čertík <ondrej@certik.us>
2023-09-02 19:53:05 -0600

    Install omp

--- ci/build_lfortran.sh
+++ ci/build_lfortran.sh
@@ -6,7 +6,7 @@ curl -o model.dat -L https://huggingface.co/datasets/certik/fastGPT/resolve/main
 
 mkdir lf
 cd lf
-FC=lfortran cmake -DFASTGPT_BLAS=OpenBLAS -DCMAKE_BUILD_TYPE=Debug ..
+FC=lfortran CMAKE_PREFIX_PATH=$CONDA_PREFIX cmake -DFASTGPT_BLAS=OpenBLAS -DCMAKE_BUILD_TYPE=Debug ..
 make VERBOSE=1
 ln -s ../model.dat .
 time OMP_NUM_THREADS=1 OPENBLAS_NUM_THREADS=1 ./gpt2
@@ -14,7 +14,7 @@ cd ..
 
 mkdir lf-fast
 cd lf-fast
-FC="lfortran --fast" cmake -DFASTGPT_BLAS=OpenBLAS -DCMAKE_BUILD_TYPE=Release ..
+FC="lfortran --fast" CMAKE_PREFIX_PATH=$CONDA_PREFIX cmake -DFASTGPT_BLAS=OpenBLAS -DCMAKE_BUILD_TYPE=Release ..
 make VERBOSE=1 gpt2
 ln -s ../model.dat .
 time OMP_NUM_THREADS=1 OPENBLAS_NUM_THREADS=1 ./gpt2
