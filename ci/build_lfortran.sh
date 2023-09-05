184246c0a07bb28d180d676ea6f8db272187d14b
Ondřej Čertík <ondrej@certik.us>
2023-09-05 15:40:27 -0600

    CI: test fastGPT with LFortran

---
+++ ci/build_lfortran.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+
+curl -o model.dat -L https://huggingface.co/datasets/certik/fastGPT/resolve/main/model_fastgpt_124M_v1.dat
+
+mkdir lf
+cd lf
+FC=lfortran CMAKE_PREFIX_PATH=$CONDA_PREFIX cmake -DFASTGPT_BLAS=OpenBLAS -DCMAKE_BUILD_TYPE=Debug ..
+make VERBOSE=1
+ln -s ../model.dat .
+time OMP_NUM_THREADS=1 OPENBLAS_NUM_THREADS=1 ./gpt2
+time OMP_NUM_THREADS=1 OPENBLAS_NUM_THREADS=1 ./test_basic_input
+time OMP_NUM_THREADS=1 OPENBLAS_NUM_THREADS=1 ./test_more_inputs
+cd ..
+
+mkdir lf-fast
+cd lf-fast
+FC="lfortran --fast" CMAKE_PREFIX_PATH=$CONDA_PREFIX cmake -DFASTGPT_BLAS=OpenBLAS -DCMAKE_BUILD_TYPE=Release ..
+make VERBOSE=1
+ln -s ../model.dat .
+time OMP_NUM_THREADS=1 OPENBLAS_NUM_THREADS=1 ./gpt2
+time OMP_NUM_THREADS=1 OPENBLAS_NUM_THREADS=1 ./test_basic_input
+time OMP_NUM_THREADS=1 OPENBLAS_NUM_THREADS=1 ./test_more_inputs
+cd ..
